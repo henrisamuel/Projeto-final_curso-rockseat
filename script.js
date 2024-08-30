1bc2e7644ed4ab8555a41ab13a1dde4c517de88a
henri@scar <henrisamuel43@gmail.com>
2024-08-30 19:35:51 -0300

    add adjust

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function toggleMode() {
   //subistituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a img
-    img.setAttribute("src" , "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     // se não estiver no light mode,  manter a img original
-    img.setAttribute("src" , "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
 
@@ -19,13 +19,13 @@ function toggleMode() {
   html.classList.toggle("light")
 
   //pegar a tag img
-  const img = document.querySelector("#profile img")
-  //subistituir a imagem
+  const alt = document.querySelector("#profile img")
+  //subistituir o alt
   if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a img
-    img.setAttribute("alt", "Foto de um gato olhando para a tela.")
+    // se tiver light mode, adicionar o alt
+    alt.setAttribute("alt", "Foto de um gato olhando para a tela.")
   } else {
-    // se não estiver no light mode,  manter a img original
-    img.setAttribute("alt", "foto de perfil virando pó")
+    // se não estiver no light mode,  manter o alt original
+    alt.setAttribute("alt", "foto de perfil virando pó")
   }
 }
