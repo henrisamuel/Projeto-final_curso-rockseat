6e05514270c6a5e6d5cf01330174312725b8763a
henri@scar <henrisamuel43@gmail.com>
2024-08-30 21:19:08 -0300

    add adjust complet

--- script.js
+++ script.js
@@ -8,24 +8,10 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a img
     img.setAttribute("src", "./assets/avatar-light.jpeg")
+    img.setAttribute("alt", "Foto de um perfil com detalhes de anjo.")
   } else {
     // se não estiver no light mode,  manter a img original
     img.setAttribute("src", "./assets/avatar.jpeg")
-  }
-}
-
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
-
-  //pegar a tag img
-  const imagem = document.querySelector("#profile img")
-  //subistituir o alt
-  if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar o alt
-    imagem.setAttribute("alt", "Foto de um gato olhando para a tela.")
-  } else {
-    // se não estiver no light mode,  manter o alt original
-    imagem.setAttribute("alt", "foto de perfil virando pó")
+    img.setAttribute("alt", "foto de perfil embassada.")
   }
 }
